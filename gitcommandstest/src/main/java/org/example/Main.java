b6a47cade7a7c855de4476418f2e5f20428bd60f
Tahir Iqbal <mtahiriqbaldeveloper@gmail.com>
2023-04-28 16:08:51 +0200

    fixup adding empty lines

--- gitcommandstest/src/main/java/org/example/Main.java
+++ gitcommandstest/src/main/java/org/example/Main.java
@@ -7,8 +7,10 @@ public class Main {
         // Press ⌥⏎ with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
+
         // Press ⌃R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
+
             // Press ⌃D to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing ⌘F8.
             System.out.println("i = " + i);
